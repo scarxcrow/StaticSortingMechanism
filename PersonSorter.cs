3ffe22a86433061a8156fb3ff0083042d4f89a69
scarxcrow <mihail.stoilkov@gmail.com>
2024-08-11 19:11:14 +0300

    Add files via upload

---
+++ PersonSorter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingMechanism_Static
+{
+    public class PersonSorter
+    {
+        public void Sort(Person[] people, Comparison<Person> comparison)
+        {
+            for (int i = 0; i < people.Length - 1; i++)
+            {
+                for(int j = i + 1; j < people.Length; j++)
+                {
+                    // Comparing people[i] and people[j] using the delegate method
+                    if (comparison(people[i], people[j]) > 0)
+                    {
+                        // Swapping people[i] with people[j] if they are in the wrong order
+                        Person temporary = people[i];
+                        people[i] = people[j];
+                        people[j] = temporary;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace SortingMechanism_Static
+{
+    internal class Program
+    {
+        public delegate int Comparison<T>(T x, T y);
+
+        static void Main(string[] args)
+        {
+            Person[] people =
+            {
+                new Person { Name = "Mishata", Age = 24 },
+                new Person { Name = "Denis", Age = 28 },
+                new Person { Name = "Rumqna", Age = 57 },
+                new Person { Name = "Nikolai", Age = 54 }
+            };
+
+
+            PersonSorter sorter = new PersonSorter();
+            sorter.Sort(people, CompareByAge);
+
+
+            Console.WriteLine("Sorting by age: ");
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}");
+            }
+
+
+            sorter.Sort(people, CompareByName);
+
+
+            Console.WriteLine("Sorting by name: ");
+            foreach(Person person in people)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int CompareByAge(Person x, Person y)
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+
+        static int CompareByName(Person x, Person y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
